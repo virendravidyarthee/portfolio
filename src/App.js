bebf54c12adc35e0d35fec900eb9988cce74b322
Virendra <virendravidyarthee@gmail.com>
2019-03-14 02:52:17 +0530

    Front page init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Particles from 'react-particles-js';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Particles className="Particles"
+                           canvasClassName="Particles-canvas"
+                           params={{
+                               "particles": {
+                                   "number": {
+                                       "value": 40
+                                   }
+                               }
+                           }}/>
+                <div className="Content-container">
+                    <div>
+                        <p className="Name-header">Hi, I'm Virendra.</p>
+                        <p className="Sub-title-one">Android Enthusiast.</p>
+                        <p className="Sub-title-two">Full stack developer.</p>
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
 }
 
 export default App;
