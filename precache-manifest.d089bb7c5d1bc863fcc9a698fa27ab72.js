73699c18792ffcc8621f57b07a748c4cddd7ab90
Virendra <virendravidyarthee@gmail.com>
2019-03-28 02:14:52 +0530

    Updates

---
+++ precache-manifest.d089bb7c5d1bc863fcc9a698fa27ab72.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ad001029930c0a20ac12e2048612542",
+    "url": "/portfolio/static/media/gmail.2ad00102.png"
+  },
+  {
+    "revision": "b87ac65aaf1f5fb3ec17713db3a9f89b",
+    "url": "/portfolio/static/media/github.b87ac65a.png"
+  },
+  {
+    "revision": "fde06245b41306294ac34e1848e3f9f6",
+    "url": "/portfolio/static/media/LinkedIn.fde06245.png"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "6fac7ee8f4e0e36a04d3",
+    "url": "/portfolio/static/js/main.6fac7ee8.chunk.js"
+  },
+  {
+    "revision": "c1705317c4ebf2dad851",
+    "url": "/portfolio/static/js/2.c1705317.chunk.js"
+  },
+  {
+    "revision": "6fac7ee8f4e0e36a04d3",
+    "url": "/portfolio/static/css/main.39bfceb0.chunk.css"
+  },
+  {
+    "revision": "cef53d603ba7ca7b998d081844b08591",
+    "url": "/portfolio/index.html"
+  }
+];
