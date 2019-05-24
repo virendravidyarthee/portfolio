7d690eefd74a5d9f54cbebf2c643dea4e250c67e
Virendra <virendravidyarthee@gmail.com>
2019-05-25 01:07:15 +0530

    Adding content for EE, CCU and Ideal

--- src/data/Data.js
+++ src/data/Data.js
@@ -15,35 +15,56 @@ const idealShortDescription = "Ideal 10 is the study companion app published by 
 
 export const myWorkSubtitle = "Working at a service based company allowed me to work on numerous projects. I’m not going to list all of them here, but I’ll list a few interesting/challenging ones. All of these are enterprise apps so they aren’t available to the general public and even if they are, the credentials to use them won’t be. But hey, if you can pull some strings with the right companies and have some time then feel free to run them on your own device.\n\n" +
     "Here you can see that I do have experience with Android, Node and Angular projects. There’s one more project that you can look at apart from the ones listed below. That is the website that you’re currently browsing. It’s built with react and is fully responsive.\n\n" +
-    "Disclaimer: I developed these projects while working for a company, so I am in no way trying to say that I have full ownership (legally and ethically, I can’t say that) of the said projects. I am merely stating the fact that I have made contributions to these projects. Which is a fair point.";
+    "Disclaimer: I developed these projects while working for a company, so I am in no way trying to say that I have full ownership (legally and ethically, I can’t say that) of the said projects. I am merely stating the fact that I have made contributions to these projects. Which is a fair point.\n"+
+    "Most of the projects listed here are enterprise (some require a fee to get access to) apps and websites. If you want to gain access to or see a demo of the apps, you need to contact either me or one of these service providers.";
 
 export const projectFullDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id nunc sed diam varius feugiat. Suspendisse feugiat tincidunt elementum. Phasellus euismod, lorem in congue ultricies, quam nibh posuere quam, ut tristique enim justo vitae augue. Proin posuere aliquam metus sed aliquam. Quisque mauris metus, volutpat non lorem et, suscipit tempus leo. Ut accumsan, felis id elementum hendrerit, elit urna tempor tortor, ut semper turpis urna et tortor. Donec leo augue, ullamcorper sed luctus eget, convallis nec ex.\n" +
     "\n" +
     "Mauris vitae ante convallis, venenatis purus ultrices, interdum lectus. Quisque quis lacinia purus. Donec tincidunt, nunc a ultrices fermentum, tellus leo iaculis odio, at porttitor nunc neque sed justo. Mauris non dignissim lacus. Sed ut ultricies velit, eu pellentesque leo. Nullam in vestibulum lectus, in vulputate magna. Praesent non lacinia tortor. Phasellus aliquam nunc mattis nibh ornare, vel consequat augue imperdiet. Phasellus eleifend risus ligula, sit amet finibus sapien sollicitudin ac. Praesent id ornare est. Sed ullamcorper, tellus ac volutpat ultricies, odio dui sollicitudin justo, et pellentesque turpis leo a dolor. Pellentesque pulvinar orci dolor. Fusce ullamcorper sollicitudin maximus. Integer vel ex ante. Mauris pretium tortor eu purus rhoncus tincidunt.\n" +
     "\n" +
-    "Donec feugiat nisi venenatis varius suscipit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris egestas quam non mi luctus finibus. Vivamus molestie pulvinar quam, rhoncus consectetur tortor volutpat non. Nam consequat tellus sapien, ac convallis velit congue a. Suspendisse malesuada tellus a eros interdum, non tincidunt turpis rhoncus. In augue nisl, placerat a ligula vel, luctus rhoncus purus. Praesent non porttitor lorem, non volutpat est. Aenean rutrum lorem dui, ac rhoncus orci volutpat a. Sed commodo nisi vitae magna tincidunt, at feugiat magna pellentesque. Suspendisse egestas ante a risus condimentum varius. Donec cursus hendrerit est eget tristique. Nulla hendrerit mattis enim, vitae iaculis purus laoreet nec. Vestibulum consequat ac risus sed vestibulum."
+    "Donec feugiat nisi venenatis varius suscipit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris egestas quam non mi luctus finibus. Vivamus molestie pulvinar quam, rhoncus consectetur tortor volutpat non. Nam consequat tellus sapien, ac convallis velit congue a. Suspendisse malesuada tellus a eros interdum, non tincidunt turpis rhoncus. In augue nisl, placerat a ligula vel, luctus rhoncus purus. Praesent non porttitor lorem, non volutpat est. Aenean rutrum lorem dui, ac rhoncus orci volutpat a. Sed commodo nisi vitae magna tincidunt, at feugiat magna pellentesque. Suspendisse egestas ante a risus condimentum varius. Donec cursus hendrerit est eget tristique. Nulla hendrerit mattis enim, vitae iaculis purus laoreet nec. Vestibulum consequat ac risus sed vestibulum.";
 
+const eeFullDescription = "There used to be a time when ExtraaEdge used to be available on the playstore but it has since been taken down as the owning company couldn’t successfully convince the playstore moderators as to why the app needed access to a user’s call logs and sms inbox. Which were unfortunately one of the app’s core features. The app is now being distributed via a custom website as a direct APK download.\n" +
+    "ExtraaEdge is a lead management platform for very well known Institutions in India like IIT, Sinhgad, K11 and many more. Their platform is used by these institutions to maximise the transition of cold lead to hot leads and thus improving admission stats. The features provided by the platform are:\n" +
+    "- Lead specific notes and remarks.\n" +
+    "- Counselor location tracking and analytics.\n" +
+    "- Call recording for registered leads.\n" +
+    "- Automated lead communication.\n" +
+    "- Smart caller IDs.\n" +
+    "- Weekly, monthly, yearly stats.\n" +
+    "- Geofencing.\n" +
+    "- Maps integration.\n" +
+    "\n" +
+    "Their full solution included a web app and Android and iOS apps. Where I was the incharge of developing and maintaining the Android and iOS apps. The project was very challenging as the app was heavily customisable and dynamic for specific counselors or institutions. A lot of dynamic layouts were employed for the specific client use cases. The most challenging part however was maintaining and extending the thousands of lines of legacy code behind the application.\n";
+
+const btSerialFullDescription = "Internally called Central Control Unit (CCU) is borderline IoT based project where a mobile device or a tablet is used to send and receive serial commands via Bluetooth and control a group of machines. The app is a strict enterprise app and access is granted only to the clients of the owning authorities.\n" +
+    "This app is designed for factory settings where it can be used to control a group of industrial printers with all sorts of commands ranging from daily operations to self testing. The app masks all the hardware level commands with a modern GUI where the user doesn’t have to worry about entering the right and often tedious serial port commands. It is also to be noted that the app was made to be modular from the very get go. As the app’s functionality was also later modified to be able to work with a school PA system with bluetooth speakers.\n" +
+    "The most tedious part of development in this project was the actual bluetooth interface and how to gracefully handle exceptions. Another area where keen observations were required was being able to handle the analog bluetooth devices that would be paired with the phone. They tend to throw a lot of exceptions when the app is trying to establish communications with them.\n";
+
+const idealFullDescription = "Ideal10 is a study companion app that allows grade 10 students in India to keep a copy of their syllabi in their smartphones. It is a fairly simple app with login and content delivery functionalities. The app is developed in association with Ideal Publications Inc. \n" +
+    "The project does include pretty eye catchy and animations and being a content delivery system, strong emphasis was put on the UX part of the app.\n" +
+    "The app also included a pretty interesting take on offline data persistence using Room and RxJava.";
 
 export const myWorkArray = [
     {
         projectName: "ExtraaEdge",
         image: eeLogo,
         shortDescription: eeShortDescription,
-        fullDescription: projectFullDescription,
-        siteLink: 'https://www.extraaedge.com/',
-        appLink: null
+        fullDescription: eeFullDescription,
+        siteLink: 'https://www.extraaedge.com/product/',
+        appLink: 'https://install.appcenter.ms/users/extraaedgebizspark/apps/extraaedge-sales-marketing-client-live/distribution_groups/external%20tester'
     },
     {
         projectName: "Bluetooth serial",
         image: ccuLogo,
         shortDescription: btSerialShortDescription,
-        fullDescription: projectFullDescription
+        fullDescription: btSerialFullDescription
     },
     {
         projectName: "Ideal10",
         image: idealLogo,
         shortDescription: idealShortDescription,
-        fullDescription: projectFullDescription,
+        fullDescription: idealFullDescription,
         siteLink: null,
         appLink: 'https://play.google.com/store/apps/details?id=in.ideal10.android'
     },
