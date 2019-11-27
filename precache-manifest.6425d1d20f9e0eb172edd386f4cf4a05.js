661f1bcef99c36e4d542b0ab6dea319cf0fc214d
Virendra <virendravidyarthee@gmail.com>
2019-11-27 00:38:37 -0500

    Updates

---
+++ precache-manifest.6425d1d20f9e0eb172edd386f4cf4a05.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec97df7dd43e1b9fa4a77a99f38e87cd",
+    "url": "/static/media/sc1.ec97df7d.png"
+  },
+  {
+    "revision": "59b676778aba772b292c",
+    "url": "/static/css/main.a6d199aa.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "466e0ca0ba362d4a622b",
+    "url": "/static/js/2.466e0ca0.chunk.js"
+  },
+  {
+    "revision": "64001615de0d430dd23d54af5f1e642b",
+    "url": "/static/media/extraaedge.64001615.png"
+  },
+  {
+    "revision": "a17209007a3a00134988f2116036a9bb",
+    "url": "/static/media/ideal10.a1720900.png"
+  },
+  {
+    "revision": "8a757c369f92a5958613169ed37520f5",
+    "url": "/static/media/ntu.8a757c36.png"
+  },
+  {
+    "revision": "72243243c00057bf5c0f3a25c1d4e04a",
+    "url": "/static/media/sdl.72243243.png"
+  },
+  {
+    "revision": "76c7ea5746aaf8cfec8cba62664fe9dc",
+    "url": "/static/media/cpp.76c7ea57.png"
+  },
+  {
+    "revision": "060196f5315177541c502b09851d677b",
+    "url": "/static/media/htmlcss.060196f5.png"
+  },
+  {
+    "revision": "771caad9adc47205249c985455a5f4ec",
+    "url": "/static/media/bolo_global_logo.771caad9.png"
+  },
+  {
+    "revision": "1a6580bf3295fb62cc271dc92aa7a357",
+    "url": "/static/media/sc1.1a6580bf.png"
+  },
+  {
+    "revision": "4d197d0f10674087052416b94924efaa",
+    "url": "/static/media/sc2.4d197d0f.png"
+  },
+  {
+    "revision": "dbe024d843dd18f7128df11a7df42680",
+    "url": "/static/media/sc3.dbe024d8.png"
+  },
+  {
+    "revision": "a2a8af3f96200a06862d96799517bb12",
+    "url": "/static/media/video call.a2a8af3f.png"
+  },
+  {
+    "revision": "59b676778aba772b292c",
+    "url": "/static/js/main.59b67677.chunk.js"
+  },
+  {
+    "revision": "fe16a5e2a87a2962e33e29db5ab24d72",
+    "url": "/static/media/sc2.fe16a5e2.png"
+  },
+  {
+    "revision": "38c4c621321d1319b942a64b0004bcdd",
+    "url": "/static/media/sc3.38c4c621.png"
+  },
+  {
+    "revision": "5f990d092569d2fb20f3c082d12ecfd1",
+    "url": "/static/media/sc4.5f990d09.png"
+  },
+  {
+    "revision": "6cd300004d3833b9504a863b5ca2c3dc",
+    "url": "/static/media/1.6cd30000.png"
+  },
+  {
+    "revision": "a6202f6d7782902ab83433df603a1e0a",
+    "url": "/static/media/2.a6202f6d.png"
+  },
+  {
+    "revision": "b634d280f42b04185deb7f4545dbd5f1",
+    "url": "/static/media/3.b634d280.png"
+  },
+  {
+    "revision": "944760d573d6f0c6511501b21f1895e5",
+    "url": "/static/media/eecover.944760d5.png"
+  },
+  {
+    "revision": "265285ede75daf81d913826881a7c352",
+    "url": "/static/media/idealCover.265285ed.png"
+  },
+  {
+    "revision": "54e080447124f86bf1a947a412af77f2",
+    "url": "/static/media/otCover.54e08044.png"
+  },
+  {
+    "revision": "7bd46c22e4412ff658376f3f7694f3c2",
+    "url": "/static/media/profile_circle.7bd46c22.png"
+  },
+  {
+    "revision": "fde06245b41306294ac34e1848e3f9f6",
+    "url": "/static/media/LinkedIn.fde06245.png"
+  },
+  {
+    "revision": "b87ac65aaf1f5fb3ec17713db3a9f89b",
+    "url": "/static/media/github.b87ac65a.png"
+  },
+  {
+    "revision": "2ad001029930c0a20ac12e2048612542",
+    "url": "/static/media/gmail.2ad00102.png"
+  },
+  {
+    "revision": "421facf089c681be81b38a6cf5bf839b",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.c14dbf945320743af464e00cd707af5a.js"
+  "/precache-manifest.6425d1d20f9e0eb172edd386f4cf4a05.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
