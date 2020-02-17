3bee6cbcbee910b7fe9855a7b9d63f9ebfee7671
Virendra <virendravidyarthee@gmail.com>
2020-02-17 11:40:10 -0500

    Reverting back to hashrouter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Home from "./containers/Home/Home";
 import MyWork from "./containers/MyWork/MyWork"
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+import { HashRouter, Route, Switch, Redirect } from 'react-router-dom'
 import Project from "./containers/Project/Project";
 import AboutMe from "./containers/AboutMe/AboutMe";
 import Sidebar from "./components/Sidebar/Sidebar";
@@ -18,7 +18,7 @@ class App extends Component {
 
     render() {
         return (
-            <BrowserRouter>
+            <HashRouter>
                 <div className="App">
                     <div className="Sidebar">
                         <Route path='/' component={Sidebar} />
@@ -36,7 +36,7 @@ class App extends Component {
                         </Switch>
                     </div>
                 </div>
-            </BrowserRouter>
+            </HashRouter>
         );
     }
 }
