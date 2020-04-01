7310ae71174a2bae48dde57e7206c4e1afc1f88c
Virendra <virendravidyarthee@gmail.com>
2020-04-01 01:22:49 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a218ed05f8b01a758d744dea3f1b793e.js"
+  "/precache-manifest.cb6ad35824017bafc1f45c5d42a144d8.js"
 );
 
 workbox.clientsClaim();
